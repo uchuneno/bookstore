71b8f0289a8d8d335778c3d5e8a47ddc5bc290a3
Retno Handayani <retnohandayani1809@gmail.com>
2016-07-16 09:47:44 +0700

    Add existing

---
+++ bookstore.sql
@@ -0,0 +1,19 @@
+/*
+-- Query: SELECT `books`.`id`,
+    `books`.`isbn`,
+    `books`.`title`,
+    `books`.`author`,
+    `books`.`publisher`,
+    `books`.`image`,
+    `books`.`updated_at`,
+    `books`.`created_at`
+FROM `bookstore`.`books`
+LIMIT 0, 1000
+
+-- Date: 2016-07-16 09:45
+*/
+INSERT INTO `books` (`id`,`isbn`,`title`,`author`,`publisher`,`image`,`updated_at`,`created_at`) VALUES (1,'1234','Catching Fire','James','Kinokuya','java10',NULL,NULL);
+INSERT INTO `books` (`id`,`isbn`,`title`,`author`,`publisher`,`image`,`updated_at`,`created_at`) VALUES (2,'4672','Harry Potter','Lily','Gramedia','java11',NULL,NULL);
+INSERT INTO `books` (`id`,`isbn`,`title`,`author`,`publisher`,`image`,`updated_at`,`created_at`) VALUES (3,'9463','Da Vinci Code','Daniel','Pustaka','java12',NULL,NULL);
+INSERT INTO `books` (`id`,`isbn`,`title`,`author`,`publisher`,`image`,`updated_at`,`created_at`) VALUES (6,'7391','World War Z','Daniel','Gramedia','java13','2016-07-16 02:25:06','2016-07-16 02:24:15');
+INSERT INTO `books` (`id`,`isbn`,`title`,`author`,`publisher`,`image`,`updated_at`,`created_at`) VALUES (7,'1092','Me Before You','Jojo','New York Times','java14','2016-07-16 02:36:13','2016-07-16 02:36:13');
